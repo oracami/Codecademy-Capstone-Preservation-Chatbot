5dd7643caab45a8ad303849c907e4a8fd34a9f3e
oracami <51378732+oracami@users.noreply.github.com>
2024-08-12 22:03:59 -0700

    Add files via upload
    LA Conservancy Retrieval Chatbot, initial framework

---
+++ responses.py
@@ -0,0 +1,33 @@
+response_a = """
+For more information about historic Los Angeles and how to preserve a potential resource, you can use the following tools/sites: 
+City of Los Angeles Program Overview: 
+https://planning.lacity.gov/preservation-design/program-overview
+ 
+The Historic Designation Process: 
+https://planning.lacity.gov/odocument/7e7328b1-9518-4f63-ae3a-3744514c1e43/Info%20Brief%20The%20Historic%20Designation%20Process.pdf
+ 
+Application: 
+https://planning.lacity.gov/odocument/7ae9f600-fb0b-4d75-a6b7-d23240560719/HCM%20Application%20Form_2018.pdf
+ 
+ZIMAS (for zoning, permits, and building information)
+https://zimas.lacity.org/
+"""
+response_b = """
+The LA Conservancy has a diverse Professional Services Directory to help you find companies, firms, and individuals 
+that can help you with your repair needs.
+"""
+response_c = """
+Unfortunately, there aren't many grants or resources to help with the rehabilitation of historic properties.
+The primary financial benefit to owning a historic home is the ability to sign up to the Mills At, which provides
+owners a tax credit meant to be used to maintain the home.
+"""
+
+response_d = """
+If you have a concern about a historic property being altered without a permit, please contact 
+the City of Los Angeles Office of Historic Resources.
+"""
+
+blank_spot = "preservation"
+
+
+responses = [response_a, response_b, response_c, response_d]
